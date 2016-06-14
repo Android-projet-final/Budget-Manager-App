a0fc1e4ca8932456334ec996406c052a07644c5a
dessteyu <bayembye10@gmail.com>
2016-06-14 09:18:18 +0000

    sauvegarde

--- app/src/main/java/com/ly/badiane/budgetmanager_finalandroidproject/sql/SqlHelper.java
+++ app/src/main/java/com/ly/badiane/budgetmanager_finalandroidproject/sql/SqlHelper.java
@@ -1,19 +1,33 @@
 package com.ly.badiane.budgetmanager_finalandroidproject.sql;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by badiane on 14/06/2016.
  */
-public class SqlHelper extends SQLiteDatabase{
+public class SqlHelper extends SQLiteOpenHelper {
 
+    private final static int  VersionDataBase =  1;//Verion of database
+    private final static String databaseName = "budgetmanager.db";//name of database
     //requete de creation de la table finances
-    private String tableFinances = "mois NUMBER AUTOINCREMENT PRIMARY KEY," +
+    private String tableFinancesRequette = "CREATE TABLE finances ( mois NUMBER AUTOINCREMENT PRIMARY KEY," +
             "an NUMBER , montant NUMBER, categories TEXT, note TEXT," +
-            "frequences TEXT";
+            "frequences TEXT)";
+    private String updateDroper = "DROP TABLE IF EXISTS finances";
 
+    public SqlHelper (Context context){
+        super(context,databaseName,null,VersionDataBase);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tableFinancesRequette);
+    }
 
-    SqlHelper() {
-        super();
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL(updateDroper);
+        onCreate(db);
     }
 }
