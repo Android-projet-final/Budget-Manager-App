09b5ca110ee2c217623a7d39c5afcbf456404af0
dessteyu <bayembye10@gmail.com>
2016-06-14 08:41:24 +0000

    sauvegarde

---
+++ app/src/main/java/com/ly/badiane/budgetmanager_finalandroidproject/sql/SqlHelper.java
@@ -0,0 +1,12 @@
+package com.ly.badiane.budgetmanager_finalandroidproject.sql;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by badiane on 14/06/2016.
+ */
+public class SqlHelper extends SQLiteDatabase{
+    SqlHelper() {
+        super();
+    }
+}
