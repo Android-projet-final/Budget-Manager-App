3bbd82127a743453ae566fb2b8c4f69a4afecabc
dessteyu <bayembye10@gmail.com>
2016-06-15 21:11:54 +0000

    sauvegarde... peut tu aider je n'arrive pas a afficher le sur mon mobile

--- app/src/main/java/com/ly/badiane/budgetmanager_finalandroidproject/vues/DepenseActivity.java
+++ app/src/main/java/com/ly/badiane/budgetmanager_finalandroidproject/vues/DepenseActivity.java
@@ -2,15 +2,47 @@ package com.ly.badiane.budgetmanager_finalandroidproject.vues;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ly.badiane.budgetmanager_finalandroidproject.R;
 
 public class DepenseActivity extends Activity {
 
+    private EditText montantedi;
+    private EditText categorirs ;
+    private EditText note;
+    private Button validation;
+    private EditText freqeuecences;
+    private EditText liscateogries;
+    private EditText date ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depense);
+        setElement();
+        setAction();
+    }
+
+
+    private void setElement(){
+        montantedi = (EditText)findViewById(R.id.editmontant);
+        liscateogries = (EditText)findViewById(R.id.textlist);
+        validation = (Button)findViewById(R.id.buttonvalider);
+        date = (EditText)findViewById(R.id.editdate);
+        note = (EditText)findViewById(R.id.editnote);
+        freqeuecences = (EditText)findViewById(R.id.edittextFrequnces);
+    }
+    private void setAction(){
+        validation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
